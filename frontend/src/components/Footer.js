22d9a4fbb79cd99f1e21d67bb14f04399f466142
IRO05 <isaacedwardrobinson@gmail.com>
2025-04-26 19:01:37 -0400

    footer

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { GiHoneycomb } from 'react-icons/gi'
+import './Footer.css'
 
 const Footer = () => {
   return (
     <footer className="app-footer">
       <div className="footer-content">
         <div className="footer-links">
-          <Link to="/">Home</Link>
-          <Link to="/studywithbuddy">Study With Buddy</Link>
-          <Link to="/faq">FAQ</Link>
-          <Link to="/calendar">Calendar</Link>
-          <Link to="/pricing">Pricing</Link>
+          <Link to="/"><GiHoneycomb className="footer-icon" /> Home</Link>
+          <Link to="/studywithbuddy"><GiHoneycomb className="footer-icon" /> Study With Buddy</Link>
+          <Link to="/faq"><GiHoneycomb className="footer-icon" /> FAQ</Link>
+          <Link to="/calendar"><GiHoneycomb className="footer-icon" /> Calendar</Link>
+          <Link to="/pricing"><GiHoneycomb className="footer-icon" /> Pricing</Link>
         </div>
         <div className="footer-copyright">
             {new Date().getFullYear()} HiveMind.
